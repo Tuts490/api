de2d649f626002a0281af185b73b6606e1582d26
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-09 08:37:52 -0300

    feat: criação do repository de Paciente

---
+++ src/main/java/com/example/backend_consultas/repository/PacienteRepository.java
@@ -0,0 +1,5 @@
+package com.example.backend_consultas.repository;
+import com.example.backend_consultas.model.Paciente;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+}
