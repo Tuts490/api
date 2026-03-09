a6dffa9792ad4f01ce8d0096786ebc0e6949aaf4
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-09 08:41:38 -0300

    feat: criação do controller de Paciente

---
+++ src/main/java/com/example/backend_consultas/controller/PacienteController.java
@@ -0,0 +1,26 @@
+package com.example.backend_consultas.controller;
+import com.example.backend_consultas.model.Paciente;
+import com.example.backend_consultas.service.PacienteService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+@RestController
+@RequestMapping("/pacientes")
+@CrossOrigin
+public class PacienteController {
+    private final PacienteService service;
+    public PacienteController(PacienteService service) {
+        this.service = service;
+    }
+    @PostMapping
+    public Paciente criar(@RequestBody Paciente paciente) {
+        return service.salvar(paciente);
+    }
+    @GetMapping
+    public List<Paciente> listar() {
+        return service.listar();
+    }
+    @GetMapping("/{id}")
+    public Paciente buscarPorId(@PathVariable Long id) {
+        return service.buscarPorId(id);
+    }
+}
