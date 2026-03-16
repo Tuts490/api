f4625362103e7a69d03e2fad1902c7d443a49e14
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-16 09:54:19 -0300

    feat: implementar update (PUT) e delete na entidade Especialidade

--- src/main/java/com/example/backend_consultas/controller/PacienteController.java
+++ src/main/java/com/example/backend_consultas/controller/PacienteController.java
@@ -7,20 +7,35 @@ import java.util.List;
 @RequestMapping("/pacientes")
 @CrossOrigin
 public class PacienteController {
+
     private final PacienteService service;
+
     public PacienteController(PacienteService service) {
         this.service = service;
     }
+
     @PostMapping
     public Paciente criar(@RequestBody Paciente paciente) {
         return service.salvar(paciente);
     }
+
     @GetMapping
     public List<Paciente> listar() {
         return service.listar();
     }
+
     @GetMapping("/{id}")
     public Paciente buscarPorId(@PathVariable Long id) {
         return service.buscarPorId(id);
+    }
+
+    @PutMapping("/{id}")
+    public Paciente atualizar(@PathVariable Long id, @RequestBody Paciente paciente) {
+        return service.atualizar(id, paciente);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id) {
+        service.deletar(id);
     }
 }
